23587bab0fd2c7465f3c830e2052e284e9f01f41
Xoaquin Sanchez Varsallona <xsanchezvarsallona@mail.austral.edu.ar>
2025-05-23 16:20:46 -0300

    Added realtime changes in frontend.

--- front/src/pages/Dashboard.tsx
+++ front/src/pages/Dashboard.tsx
@@ -1,13 +1,26 @@
+import { useEffect, useState } from "react";
 import Balance from "../components/Dashboard/Balance";
 import QuickActions from "../components/Dashboard/QuickActions";
-import RecentActivity from "../components/Dashboard/RecentActivity";
+import { TransactionType } from "../components/Transaction/Filters";
+import TransactionList from "../components/Transaction/TransactionList";
 
 export default function Dashboard() {
+  const [fromDate, setFromDate] = useState("");
+
+
+  useEffect(() => {
+    const today = new Date();
+    const threeMonthsAgo = new Date();
+    threeMonthsAgo.setMonth(today.getMonth() - 3);
+    const from = threeMonthsAgo.toISOString().slice(0, 10);
+    setFromDate(from);
+  }, []);
+
   return (
     <div style={{ maxWidth: 480, margin: '0 auto', padding: 16 }}>
       <Balance />
       <QuickActions />
-      <RecentActivity />
+      <TransactionList type={TransactionType.All} fromDate={fromDate} toDate={null} />
     </div>
   );
 } 
