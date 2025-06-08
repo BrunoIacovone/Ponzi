2bd1cc831a1bbdf223642a8bdc57a394b79c20ac
Xoaquin Sanchez Varsallona <xsanchezvarsallona@mail.austral.edu.ar>
2025-06-08 00:19:41 -0300

    changes in firebase.ts

--- back/src/firebase.ts
+++ back/src/firebase.ts
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === 'test') {
     databaseURL: process.env.FIREBASE_DATABASE_URL_EMULATOR,
   });
 } else {
-  const serviceAccountPath = path.resolve(__dirname, '../firebase-service-account.json');
+  const serviceAccountPath = path.join(__dirname, 'firebase-service-account.json');
 
   if (!fs.existsSync(serviceAccountPath)) {
     throw new Error(
