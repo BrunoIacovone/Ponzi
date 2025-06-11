a0365b22610d9d003e1b42881d9508fc0a790c96
Marcos <mcarrizoaslanian@mail.austral.edu.ar>
2025-06-11 16:33:25 -0300

    fix

--- front/test/specs/test.e2e.ts
+++ front/test/specs/test.e2e.ts
@@ -1,21 +1,21 @@
-import { expect, browser, $ } from '@wdio/globals'
+import { expect, browser, $ } from '@wdio/globals';
 
 describe('Mi app con Capacitor', () => {
   it('debería cargar correctamente el WebView', async () => {
-    const contexts = await browser.getContexts()
-    console.log('Contextos disponibles:', contexts)
+    const contexts = await browser.getContexts();
+    console.log('Contextos disponibles:', contexts);
 
     // Cambiar al contexto WEBVIEW
-    const webviewContext = contexts.find((ctx) => ctx.includes('WEBVIEW'))
-    expect(webviewContext).toBeDefined()
-    await browser.switchContext(webviewContext!)
+    const webviewContext = contexts.find((ctx) => ctx.includes('WEBVIEW'));
+    expect(webviewContext).toBeDefined();
+    await browser.switchContext(webviewContext!);
 
     // Validar que algo se muestre dentro del HTML
-    const body = await $('body')
-    await expect(body).toBeDisplayed()
+    const body = await $('body');
+    await expect(body).toBeDisplayed();
 
     // O podés buscar por un texto visible
     // const titulo = await $('h1=Inicio') // si tenés un <h1>Inicio</h1>
     // await expect(titulo).toBeDisplayed()
-  })
-})
+  });
+});
